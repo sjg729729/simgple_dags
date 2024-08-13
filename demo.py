ffabf77f22a29d677432744a2155d81775e63055
신진건 <82808821+sjg729729@users.noreply.github.com>
2024-08-14 01:03:26 +0900

    Update demo.py

--- demo.py
+++ demo.py
@@ -7,7 +7,7 @@ from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 
 # ① A DAG represents a workflow, a collection of tasks
-with DAG(dag_id="demo", start_date=datetime(2023, 10, 1), schedule="@daily") as dag:
+with DAG(dag_id="demo", start_date=datetime(2023, 10, 1), schedule="@daily", catchup=False) as dag:
 
     # ② Tasks are represented as operators
     hello = BashOperator(task_id="hello", bash_command="echo hello")
